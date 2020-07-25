388a25351fac432b69d95fa107f7b9053ac0699d
ayushcs <ayushsrivastavacs@gmail.com>
2020-07-25 22:14:03 +0530

    Updates

---
+++ precache-manifest.d56e7ab582ba289edd945d3a66e0712e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f981b0e959b9229bb31951e8fc65909",
+    "url": "/MovieManagementSystem/index.html"
+  },
+  {
+    "revision": "d6236bd48a5ee7b8d74d",
+    "url": "/MovieManagementSystem/static/js/2.2349fee3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MovieManagementSystem/static/js/2.2349fee3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa5ec8bfb730fe9214c",
+    "url": "/MovieManagementSystem/static/js/main.04efa68a.chunk.js"
+  },
+  {
+    "revision": "5a6f3bda6b6b9f768ef1",
+    "url": "/MovieManagementSystem/static/js/runtime-main.b01db43d.js"
+  }
+]);
